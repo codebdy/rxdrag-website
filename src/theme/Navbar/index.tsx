c57d55e46576d6e43a71553787678806bba385d7
Water.Li <lizongbin@hotmail.com>
2021-07-23 15:15:40 +0800

    backup

--- src/theme/Navbar/index.tsx
+++ src/theme/Navbar/index.tsx
@@ -37,61 +37,63 @@ export default function NavBar(props:{}){
   useLockBodyScroll(sidebarShown)
 
   return(
-    <div className="container">
-      <nav className={clsx("navbar", styles.navbar,  {
-        "navbar-sidebar--show": sidebarShown,
-      })} style={{boxShadow:'none'}}>
-        <div
-          aria-label="Navigation bar toggle"
-          className="navbar__toggle"
-          role="button"
-          tabIndex={0}
-          onClick={showSidebar}
-          onKeyDown={showSidebar}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 30 30"
-            role="img"
-            focusable="false"
+    <div className = {styles.shell}>
+      <div className={clsx("container")}>
+        <nav className={clsx("navbar", styles.navbar,  {
+          "navbar-sidebar--show": sidebarShown,
+        })} style={{boxShadow:'none'}}>
+          <div
+            aria-label="Navigation bar toggle"
+            className="navbar__toggle"
+            role="button"
+            tabIndex={0}
+            onClick={showSidebar}
+            onKeyDown={showSidebar}
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              strokeWidth="2"
-              d="M4 7h22M4 15h22M4 23h22"
-            />
-          </svg>
-        </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              role="img"
+              focusable="false"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                strokeWidth="2"
+                d="M4 7h22M4 15h22M4 23h22"
+              />
+            </svg>
+          </div>
 
 
-        <div className="navbar__inner">
-          <div className={clsx("navbar__items", {[styles.mobile]:!isDesktop})}>
-            <img className={styles.logo} src={logo.src} />
-            <a className={clsx("navbar__brand", styles.brand)}>{title}</a>
-            {
-              leftItems.map((item, i) => (
-                <a key={item.to + i} className="navbar__item navbar__link" href={item.to}>
-                {item.label}
-              </a>
-              ))
-            }
+          <div className="navbar__inner">
+            <div className={clsx("navbar__items", {[styles.mobile]:!isDesktop})}>
+              <img className={styles.logo} src={logo.src} />
+              <a className={clsx("navbar__brand", styles.brand)}>{title}</a>
+              {
+                leftItems.map((item, i) => (
+                  <a key={item.to + i} className="navbar__item navbar__link" href={item.to}>
+                  {item.label}
+                </a>
+                ))
+              }
+            </div>
+            <div className="navbar__items navbar__items--right">
+              {
+                rightItems.map((item, i) => (
+                  <a key={item.to + i} className="navbar__item navbar__link" href={item.to}>
+                  {item.src ? <img src={item.src} style={{marginTop:'6px'}} /> : item.label}
+                </a>
+                ))
+              }
+            </div>
           </div>
-          <div className="navbar__items navbar__items--right">
-            {
-              rightItems.map((item, i) => (
-                <a key={item.to + i} className="navbar__item navbar__link" href={item.to}>
-                {item.src ? <img src={item.src} style={{marginTop:'6px'}} /> : item.label}
-              </a>
-              ))
-            }
-          </div>
-        </div>
-        <NavSideBar title = {title} logo = {logo} items= {items} onHide={hideSidebar} />
-      </nav>
+          <NavSideBar title = {title} logo = {logo} items= {items} onHide={hideSidebar} />
+        </nav>
+      </div>
     </div>
   )
 }
