d87246976a71d535ea7b5131a68e7950f6dc537b
Water.Li <lizongbin@hotmail.com>
2021-07-24 13:35:04 +0800

    add footer nav

--- src/components/FooterAvatar/index.tsx
+++ src/components/FooterAvatar/index.tsx
@@ -6,7 +6,22 @@ import styles from "./styles.module.css"
 export default function FooterAvatar(){
   return (
     <div className = {styles.footAvatar}>
+      <div className = {styles.logo}>
+        <span className = {styles.ruxin}>rx</span><span>Drag</span>
+      </div>
+      <div className = {styles.footerNav}>
+        <ul>
+          <li> <a href="#">首页</a> </li>
+          <li> <a href="#">入门</a> </li>
+          <li> <a href="#">生态圈</a> </li>
+          <li> <a href="#">应用</a> </li>
+          <li> <a href="#">GITHUB</a> </li>          
+          <li> <a href="#">交流群</a> </li>
+        </ul>
+
+      </div>
       <div className = {styles.layout}>
+
         <div>
           <a href="https://github.com/rxdrag" target="_blank" >
             <img className = {clsx(styles.maskSquircle, styles.mask)} src="img/avatar.jpg" />
