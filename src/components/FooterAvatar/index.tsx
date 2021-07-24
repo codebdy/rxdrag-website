65d904800b55a7d2348ec408cffc4f375010596d
Water.Li <lizongbin@hotmail.com>
2021-07-24 12:52:39 +0800

    add github link to avatar

--- src/components/FooterAvatar/index.tsx
+++ src/components/FooterAvatar/index.tsx
@@ -7,13 +7,15 @@ export default function FooterAvatar(){
     <div className = {styles.footAvatar}>
       <div className = {styles.layout}>
         <div>
-          <img className = {clsx(styles.maskSquircle, styles.mask)} src="img/avatar.jpg" />
+          <a href="https://github.com/rxdrag" target="_blank" >
+            <img className = {clsx(styles.maskSquircle, styles.mask)} src="img/avatar.jpg" />
+          </a>
         </div>
         <div className={styles.textArea}>
           <div className={styles.grayText}>
             Designed and developed by
           </div>
-          <div>悠闲的水</div>
+          <div><a target="_blank" href="https://github.com/rxdrag">悠闲的水</a></div>
         </div>
       </div>
       <div className={styles.beian}>
